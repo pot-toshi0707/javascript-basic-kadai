177be83c0fc643d02fad3f318fff7b24d9d5aee0
pot-toshi0707 <pot.toshi0707@gmail.com>
2025-04-06 19:59:46 -0700

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,11 @@
+
+// 現在の日時を取得するためのDateオブジェクトのインスタンスを作成
+const now = new Date();
+
+const year = now.getFullYear();
+const month = now.getMonth() + 1;
+const date = now.getDate();
+
+const dateString = `${year}年${month}月${date}日`;
+
+console.log(dateString);
